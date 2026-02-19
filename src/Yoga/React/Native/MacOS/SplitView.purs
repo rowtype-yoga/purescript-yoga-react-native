3ff796a29dfda28925e60c72d28120281edcbce9
Mark <mark.eibes@gmail.com>
2026-02-20 00:11:22 +0100

    Rewrite SplitView in pure PureScript using PanResponder + Hooks

--- src/Yoga/React/Native/MacOS/SplitView.purs
+++ src/Yoga/React/Native/MacOS/SplitView.purs
@@ -8,7 +8,7 @@ import Data.Tuple.Nested ((/\))
 import Effect.Uncurried (mkEffectFn2)
 import Effect.Unsafe (unsafePerformEffect)
 import React.Basic (JSX)
-import React.Basic.Events (EventHandler, handler_)
+import React.Basic.Events (EventHandler, handler, handler_, unsafeEventFn)
 import React.Basic.Hooks as React
 import Unsafe.Coerce (unsafeCoerce)
 import Yoga.React (component)
@@ -18,7 +18,8 @@ import Yoga.React.Native.Style as Style
 
 type SplitViewProps =
   { initialPosition :: Number
-  , minPaneWidth :: Number
+  , minLeftWidth :: Number
+  , minRightWidth :: Number
   , left :: JSX
   , right :: JSX
   }
@@ -29,10 +30,17 @@ splitView = component "SplitView" \props -> React.do
   hovered /\ setHovered <- React.useState' false
   widthRef <- React.useRef props.initialPosition
   startWidthRef <- React.useRef 0.0
+  containerWidthRef <- React.useRef 1000.0
 
   React.useEffect sidebarWidth do
     React.writeRef widthRef sidebarWidth
     pure (pure unit)
+
+  let dividerWidth = 5.0
+  let clampWidth w = do
+        cw <- React.readRef containerWidthRef
+        let maxW = cw - props.minRightWidth - dividerWidth
+        pure (clamp props.minLeftWidth maxW w)
 
   pr <- React.useMemo unit \_ -> unsafePerformEffect $ Pan.create
     { onStartShouldSetPanResponder: mkEffectFn2 \_ _ -> pure true
@@ -42,15 +50,18 @@ splitView = component "SplitView" \props -> React.do
         React.writeRef startWidthRef w
     , onPanResponderMove: mkEffectFn2 \_ (gs :: Pan.GestureState) -> do
         sw <- React.readRef startWidthRef
-        let newWidth = max props.minPaneWidth (sw + gs.dx)
+        newWidth <- clampWidth (sw + gs.dx)
         setSidebarWidth newWidth
     }
 
   let handlers = toResponderHandlers (Pan.panHandlers pr)
   let dColor = if hovered then "rgba(128,128,128,0.6)" else "rgba(128,128,128,0.3)"
 
+  let onLayout = handler (unsafeEventFn layoutWidth) \w ->
+        React.writeRef containerWidthRef w
+
   pure do
-    view { style: tw "flex-1 flex-row" }
+    view { style: tw "flex-1 flex-row", onLayout }
       [ view
           { style: Style.style { width: sidebarWidth, overflow: "hidden" }
           , allowsVibrancy: true
@@ -81,6 +92,9 @@ splitView = component "SplitView" \props -> React.do
 
 -- PanResponder.panHandlers returns responder-named event handlers
 -- but the PureScript type uses pan-prefixed names, so we coerce
+layoutWidth :: forall a. a -> Number
+layoutWidth e = (unsafeCoerce e).nativeEvent.layout.width
+
 toResponderHandlers :: Pan.PanResponderHandlers -> ResponderHandlers
 toResponderHandlers = unsafeCoerce
 
