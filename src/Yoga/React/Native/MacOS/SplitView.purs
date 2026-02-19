b14dca4421ffd2fd6afa7df4f7ca84a3996be738
Mark <mark.eibes@gmail.com>
2026-02-20 00:07:14 +0100

    Restore working wrapper Views with allowsVibrancy, 5px divider

--- src/Yoga/React/Native/MacOS/SplitView.purs
+++ src/Yoga/React/Native/MacOS/SplitView.purs
@@ -1,27 +1,95 @@
 module Yoga.React.Native.MacOS.SplitView
-  ( nativeSplitView
-  , NativeSplitViewAttributes
-  , DividerPosition
+  ( splitView
   ) where
 
-import Prelude (Unit)
-import Effect (Effect)
-import React.Basic (ReactComponent)
-import Yoga.React.Native.Attributes (BaseAttributes)
-import Yoga.React.Native.Internal (FFINativeComponent, createNativeElement)
+import Prelude
 
-foreign import splitViewImpl :: forall props. ReactComponent props
+import Data.Tuple.Nested ((/\))
+import Effect.Uncurried (mkEffectFn2)
+import Effect.Unsafe (unsafePerformEffect)
+import React.Basic (JSX)
+import React.Basic.Events (EventHandler, handler_)
+import React.Basic.Hooks as React
+import Unsafe.Coerce (unsafeCoerce)
+import Yoga.React (component)
+import Yoga.React.Native (view, tw)
+import Yoga.React.Native.PanResponder as Pan
+import Yoga.React.Native.Style as Style
 
-nativeSplitView :: FFINativeComponent NativeSplitViewAttributes
-nativeSplitView = createNativeElement splitViewImpl
-
-type DividerPosition = { position :: Number }
-
-type NativeSplitViewAttributes = BaseAttributes
-  ( initialPosition :: Number
+type SplitViewProps =
+  { initialPosition :: Number
   , minPaneWidth :: Number
-  , dividerThickness :: Number
-  , dividerColor :: String
-  , dividerHoverColor :: String
-  , onDividerPosition :: DividerPosition -> Effect Unit
-  )
+  , left :: JSX
+  , right :: JSX
+  }
+
+splitView :: SplitViewProps -> JSX
+splitView = component "SplitView" \props -> React.do
+  sidebarWidth /\ setSidebarWidth <- React.useState' props.initialPosition
+  hovered /\ setHovered <- React.useState' false
+  widthRef <- React.useRef props.initialPosition
+  startWidthRef <- React.useRef 0.0
+
+  React.useEffect sidebarWidth do
+    React.writeRef widthRef sidebarWidth
+    pure (pure unit)
+
+  pr <- React.useMemo unit \_ -> unsafePerformEffect $ Pan.create
+    { onStartShouldSetPanResponder: mkEffectFn2 \_ _ -> pure true
+    , onMoveShouldSetPanResponder: mkEffectFn2 \_ _ -> pure true
+    , onPanResponderGrant: mkEffectFn2 \_ _ -> do
+        w <- React.readRef widthRef
+        React.writeRef startWidthRef w
+    , onPanResponderMove: mkEffectFn2 \_ (gs :: Pan.GestureState) -> do
+        sw <- React.readRef startWidthRef
+        let newWidth = max props.minPaneWidth (sw + gs.dx)
+        setSidebarWidth newWidth
+    }
+
+  let handlers = toResponderHandlers (Pan.panHandlers pr)
+  let dColor = if hovered then "rgba(128,128,128,0.6)" else "rgba(128,128,128,0.3)"
+
+  pure do
+    view { style: tw "flex-1 flex-row" }
+      [ view
+          { style: Style.style { width: sidebarWidth, overflow: "hidden" }
+          , allowsVibrancy: true
+          }
+          [ props.left ]
+      , view
+          { style: Style.style { width: 5.0, cursor: "col-resize", alignItems: "center", justifyContent: "center" }
+          , onStartShouldSetResponder: handlers.onStartShouldSetResponder
+          , onMoveShouldSetResponder: handlers.onMoveShouldSetResponder
+          , onResponderGrant: handlers.onResponderGrant
+          , onResponderMove: handlers.onResponderMove
+          , onResponderRelease: handlers.onResponderRelease
+          , onResponderTerminate: handlers.onResponderTerminate
+          , onResponderTerminationRequest: handlers.onResponderTerminationRequest
+          , onMouseEnter: handler_ (setHovered true)
+          , onMouseLeave: handler_ (setHovered false)
+          }
+          [ view
+              { style: Style.style { width: 1.0, height: "100%", backgroundColor: dColor }
+              , pointerEvents: "none"
+              }
+              (mempty :: Array JSX)
+          ]
+      , view
+          { style: tw "flex-1" <> Style.style { overflow: "hidden" } }
+          [ props.right ]
+      ]
+
+-- PanResponder.panHandlers returns responder-named event handlers
+-- but the PureScript type uses pan-prefixed names, so we coerce
+toResponderHandlers :: Pan.PanResponderHandlers -> ResponderHandlers
+toResponderHandlers = unsafeCoerce
+
+type ResponderHandlers =
+  { onStartShouldSetResponder :: EventHandler
+  , onMoveShouldSetResponder :: EventHandler
+  , onResponderGrant :: EventHandler
+  , onResponderMove :: EventHandler
+  , onResponderRelease :: EventHandler
+  , onResponderTerminate :: EventHandler
+  , onResponderTerminationRequest :: EventHandler
+  }
