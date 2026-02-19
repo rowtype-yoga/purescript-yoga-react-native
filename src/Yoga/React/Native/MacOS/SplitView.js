24f40df794f9788bb2f38622b7f34d412dfd3347
Mark <mark.eibes@gmail.com>
2026-02-20 00:04:15 +0100

    Restore PanResponder for drag, 1px divider with absolute 9px hit area

--- src/Yoga/React/Native/MacOS/SplitView.js
+++ src/Yoga/React/Native/MacOS/SplitView.js
@@ -15,7 +15,6 @@ export const splitViewImpl = React.forwardRef((props, ref) => {
   } = props;
 
   const [sidebarWidth, setSidebarWidth] = useState(initialPosition);
-  const [dragging, setDragging] = useState(false);
   const [hovered, setHovered] = useState(false);
   const widthRef = useRef(sidebarWidth);
   widthRef.current = sidebarWidth;
@@ -27,20 +26,15 @@ export const splitViewImpl = React.forwardRef((props, ref) => {
       onMoveShouldSetPanResponder: () => true,
       onPanResponderGrant: () => {
         startWidth.current = widthRef.current;
-        setDragging(true);
       },
       onPanResponderMove: (_, gesture) => {
         const newWidth = Math.max(minPaneWidth, startWidth.current + gesture.dx);
         setSidebarWidth(newWidth);
       },
       onPanResponderRelease: () => {
-        setDragging(false);
         if (typeof onDividerPosition === "function") {
           onDividerPosition({ position: widthRef.current })();
         }
-      },
-      onPanResponderTerminate: () => {
-        setDragging(false);
       },
     })
   );
@@ -51,7 +45,6 @@ export const splitViewImpl = React.forwardRef((props, ref) => {
 
   const dColor = dividerColor || "rgba(128,128,128,0.3)";
   const dHoverColor = dividerHoverColor || "rgba(128,128,128,0.6)";
-  const active = dragging || hovered;
 
   return React.createElement(
     View,
@@ -63,26 +56,24 @@ export const splitViewImpl = React.forwardRef((props, ref) => {
     leftChild && React.cloneElement(leftChild, {
       style: [leftChild.props.style, { width: sidebarWidth }],
     }),
-    // 1px visible line with wider invisible PanResponder hit area
     React.createElement(View, {
+      ...panRef.current.panHandlers,
+      onMouseEnter: () => setHovered(true),
+      onMouseLeave: () => setHovered(false),
       style: {
-        width: dividerThickness,
-        backgroundColor: active ? dHoverColor : dColor,
+        width: 5,
+        cursor: "col-resize",
+        alignItems: "center",
+        justifyContent: "center",
       },
     },
       React.createElement(View, {
-        ...panRef.current.panHandlers,
-        onMouseEnter: () => setHovered(true),
-        onMouseLeave: () => setHovered(false),
         style: {
-          position: "absolute",
-          top: 0,
-          bottom: 0,
-          left: -4,
-          right: -4,
-          cursor: "col-resize",
-          zIndex: 10,
+          width: dividerThickness,
+          height: "100%",
+          backgroundColor: hovered ? dHoverColor : dColor,
         },
+        pointerEvents: "none",
       })
     ),
     rightChild && React.cloneElement(rightChild, {
